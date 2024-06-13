04748a31eb39b65f3b2d38b80af2ff6e26172a0c
Fabio R <fabioraffa01@gmail.com>
2024-06-13 15:40:29 +0200

    Added metod multiplication

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+public float multiplication (float val1, float val2 ){
+    float risultato = val1 * val2;
+    return risultato;
+    }
 }
