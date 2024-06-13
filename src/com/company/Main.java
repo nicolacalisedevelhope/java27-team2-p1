f80500751655ca81142899dd21b420efb6cfd1af
Simone Cuccu <simone.cuccu3@gmail.com>
2024-06-13 16:01:03 +0200

    Aggiunta Somma Aggiunta Sottrazione

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    }
+
+    public static float sum(float val1, float val2){
+        float sumResult = val1 + val2;
+        return sumResult;
+    }
+
+    public static float subtraction(float val1, float val2){
+        float subtractionResult = val1 - val2;
+        return subtractionResult;
     }
 }
