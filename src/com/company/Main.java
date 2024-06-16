80414fe4a7c553d31fcee45fc4cc843887b8cd5e
Fabio R <fabioraffa01@gmail.com>
2024-06-16 15:53:33 +0200

    Added print of result in metod, added case of second value equal 0, and changed metod in static

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,14 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
-
-public float division (float val1, float val2 ){
-    float risultato = val1 / val2;
-    return risultato;
+    public static void division(float val1, float val2) {
+        if (val2 == 0) {
+            System.out.println("Errore, non può essere effettuata divisione per zero!");
+        }
+        else {
+            float risultato = val1 / val2;
+            System.out.println("Il risultato della divisione è: " + risultato);
+        }
     }
 
 }
